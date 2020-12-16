0ec526d52c7261097ef4e43ae97c959f462e94aa
9100bo <65898377+9100bo@users.noreply.github.com>
2020-12-16 23:05:01 +0800

    12/16

---
+++ js/script.js
@@ -0,0 +1,27 @@
+/* Navbar */
+const navbar = document.querySelector('.js-custom-navbar');
+const menuIcon = document.querySelector('.js-menu-icon');
+const menuIconClose = document.querySelector('.js-menu-icon span');
+
+
+menuIcon.addEventListener('click', e => {
+  e.preventDefault();
+  navbar.classList.toggle("open");
+  menuIconClose.classList.toggle("fa-times");
+});
+
+/* Staff */
+const horizontalAccordions = $(".accordion.width");
+
+horizontalAccordions.each((index, element) => {
+	const accordion = $(element);
+  const collapse = accordion.find(".collapse");
+  const bodies = collapse.find("> *");
+  accordion.height(accordion.height());  
+  bodies.width(bodies.eq(0).width());
+  collapse.not(".show").each((index, element) => {
+  	$(element).parent().find("[data-toggle='collapse']").addClass("collapsed");
+  });
+});
+
+/* Catalog */
